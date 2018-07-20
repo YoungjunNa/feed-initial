0a3b56fdc993b9b7e0efde6642272a58b9b587be
Youngjun Na <ruminoreticulum@gmail.com>
2018-07-20 17:04:09 +0900

    distribute the 1st package

---
+++ R/feed.search.R
@@ -0,0 +1,19 @@
+#' feed.search
+#'
+#' This function search a keyword at feedipedia.org.
+#' @param keyword A keyword that you want to find.
+#' @keywords feed, feedipedia
+#' @export
+#' @examples
+#' feed.search("alfalfa")
+
+feed.search <- function(keyword) {
+  url <- paste0("https://www.feedipedia.org/search/node/",keyword)
+  html <- read_html(url, encoding="UTF-8")
+  
+  result <- html %>% html_nodes("h3") %>% html_children() %>% html_text() # search results
+  url <- html %>% html_nodes("h3 a") %>% html_attr("href")
+  
+  df <- data.frame(result,url)
+  print(df)
+}

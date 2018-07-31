78fea8933f658470b876b2a9cc2bd8ad2dc2a8f0
Youngjun Na <ruminoreticulum@gmail.com>
2018-07-31 13:23:19 +0900

    add nrc beef dataset

---
+++ data-raw/feed.nrc.beef.R
@@ -0,0 +1,7 @@
+#' Feed chemical composition from NRC beef 2016
+#'
+#' @format A data frame with 219 rows and 57 variables:
+
+feed.nrc.beef <-
+  read.csv("feed.nrc.beef.txt")
+devtools::use_data(experiment1)
